93296217b44ec1c66e24e9c6e6003a64fb99a3c2
ecekpo <ememekpo@yahoo.com>
2022-09-14 11:07:26 +0100

    Add Quote.js

---
+++ src/Component/Quote.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './CSS/Quote.css';
+
+function Quote() {
+  return (
+    <div className="quote">
+      <p>
+        Mathematics is not about numbers, equations, computations, or
+        <br />
+        algorithms: it is about understanding.
+      </p>
+      <p className="name">- William Paul Thurston</p>
+    </div>
+  );
+}
+
+export default Quote;
