dbf68ed0e16316cd5174203cd4ec5386f357497d
ecekpo <ememekpo@yahoo.com>
2022-09-14 11:05:57 +0100

    Add Home.js

---
+++ src/Component/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './CSS/Home.css';
+
+function Home() {
+  return (
+    <div className="home-page">
+      <h1 className="intro-page">Welcome to our Page!</h1>
+      <h3 className="home-title">Calculator makes simple</h3>
+      <p className="description">
+        Getting started with the Calculator by clicking on the calculator link above.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
